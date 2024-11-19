739dd5145c1bc626fe2c99834d0d4e392ab22625
sangmaaaaan <matthew10164@gmail.com>
2024-11-19 20:49:22 +0900

    ✨ feat: add test page

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col items-center min-h-[100vh] px-4">
+      <p>HomePage</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/Login/LoginRedirectPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoginRedirectPage = () => {
+  return (
+    <div className="flex flex-col items-center min-h-[100vh] px-4">
+      <p>LoginRedirectPage</p>
+    </div>
+  )
+}
+export default LoginRedirectPage
