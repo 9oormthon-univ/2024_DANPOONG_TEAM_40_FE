dbd3fe64c28cdf7a5c42ca5859fbc05afec93975
alwn8918 <alwn8918@gmail.com>
2024-11-20 23:24:47 +0900

    ✨ Feat: TitleLine 컴포넌트 구현

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import TitleLine from "./TitleLine";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col items-center min-h-[100vh] px-4">
-      <p>LoginPage</p>
+    <div className="flex flex-col items-center min-h-[100vh]">
+      <TitleLine title={"모두의 길"} />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
