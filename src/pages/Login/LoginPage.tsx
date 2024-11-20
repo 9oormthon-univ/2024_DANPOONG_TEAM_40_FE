da55b42b2e2534b2b7ef41c7b7b04c2c351c3e65
alwn8918 <alwn8918@gmail.com>
2024-11-21 01:45:07 +0900

    ✨ Feat: 로그인 화면 이동

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import * as L from "./LoginPage.style";
-import TitleLine from "./TitleLine";
+import TitleLine from "./components/TitleLine";
 
 // image
 import modumong from "../../assets/modumong.svg";
 import kakaoLogin from "../../assets/kakao_login.svg";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleGuestLogin = () => {
+    navigate("/");
+  };
+
+  const handleKakaoLogin = () => {
+    navigate("/auth");
+  };
+
   return (
     <L.Container>
       <L.Image src={modumong} alt="modumong" />
       <TitleLine title={"모두의 길"} />
-      <L.KakaoLogin src={kakaoLogin} alt="kakaoLogin" />
-      <L.Login>비회원으로 시작하기</L.Login>
+      <button type="button" onClick={handleKakaoLogin}>
+        <L.KakaoLogin src={kakaoLogin} alt="kakaoLogin" />
+      </button>
+      <L.Login type="button" onClick={handleGuestLogin}>
+        비회원으로 시작하기
+      </L.Login>
     </L.Container>
   );
 };
