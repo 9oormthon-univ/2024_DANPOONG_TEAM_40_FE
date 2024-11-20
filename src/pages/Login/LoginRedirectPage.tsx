4eb37fc1b502073cd3058a2829e8be81302f2056
alwn8918 <alwn8918@gmail.com>
2024-11-21 05:01:52 +0900

    ✨ Feat: 프로필 이미지 선택 모달 구현

--- src/pages/Login/LoginRedirectPage.tsx
+++ src/pages/Login/LoginRedirectPage.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import * as L from "./LoginRedirectPage.style";
 
 // image
 import camera from "../../assets/icon/camera.svg";
+import Modal from "./components/Modal";
+import PictureModal from "./components/PictureModal";
 
 const LoginRedirectPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <L.Container>
       <L.GuideLabel>
@@ -12,11 +19,15 @@ const LoginRedirectPage = () => {
         <br />
         <span>프로필</span>을 설정해 주세요.
       </L.GuideLabel>
-      <L.Profile type="button">
+      <L.Profile type="button" onClick={openModal}>
         <img src={camera} alt="select photo" />
       </L.Profile>
       <L.NicknameInput placeholder="닉네임을 입력해 주세요." />
       <L.Button type="button">완료</L.Button>
+
+      <Modal isOpen={isModalOpen}>
+        <PictureModal onClose={closeModal} />
+      </Modal>
     </L.Container>
   );
 };
--- src/pages/Login/components/Modal.tsx
+++ src/pages/Login/components/Modal.tsx
@@ -2,10 +2,13 @@ import { ReactNode } from "react";
 import * as M from "./Modal.style";
 
 interface ModalProps {
+  isOpen: Boolean;
   children: ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
+  if (!isOpen) return null;
+
   return (
     <M.Background>
       <M.Container>{children}</M.Container>
