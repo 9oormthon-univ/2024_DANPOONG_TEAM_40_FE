3ee020eb4a97986d380f3b5db75a7beb233f61a4
alwn8918 <alwn8918@gmail.com>
2024-11-21 08:37:09 +0900

    ✨ Feat: 추천탭 Router 추가

--- src/routes/basicRouter.ts
+++ src/routes/basicRouter.ts
@@ -1,24 +1,29 @@
 import HomePage from "../pages/Home/HomePage";
 import LoginPage from "../pages/Login/LoginPage";
 import LoginRedirectPage from "../pages/Login/LoginRedirectPage";
+import RecommendPage from "../pages/Recommend/RecommendPage";
 import { TRoute } from "../types/commonTypes";
 
 export const BASIC_ROUTES_URL = {
-    home: {
-        name: '홈페이지',
-        path: () => '/',
-        component: HomePage,
-    },
-    login: {
-        name: '로그인',
-        path: () => '/login',
-        component: LoginPage
-    },
-    auth: {
-        name: '로그인 리다이렉트',
-        path: () => '/auth',
-        component: LoginRedirectPage
-    },
-    
+  home: {
+    name: "홈페이지",
+    path: () => "/",
+    component: HomePage,
+  },
+  login: {
+    name: "로그인",
+    path: () => "/login",
+    component: LoginPage,
+  },
+  auth: {
+    name: "로그인 리다이렉트",
+    path: () => "/auth",
+    component: LoginRedirectPage,
+  },
+  recommend: {
+    name: "추천",
+    path: () => "/recommend",
+    component: RecommendPage,
+  },
 } as const;
 export const BASIC_ROUTES: TRoute[] = Object.values(BASIC_ROUTES_URL);
