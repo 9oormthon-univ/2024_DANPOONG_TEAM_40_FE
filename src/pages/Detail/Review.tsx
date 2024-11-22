43ecd78bbfe54b9198778580d72dcf1b68a6cbea
alwn8918 <alwn8918@gmail.com>
2024-11-23 02:28:54 +0900

    ✨ Feat: 별점 컴포넌트 구현

--- src/pages/Detail/Review.tsx
+++ src/pages/Detail/Review.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import StarRating from "./components/StarRating";
 
 const Review = () => {
-  return <Container>Review</Container>;
+  return (
+    <Container>
+      <ReviewDesc>
+        <p>리뷰 3개</p>
+        <button type="button">리뷰 쓰기</button>
+      </ReviewDesc>
+      <StarRating totalStars={5} initialRating={4.9} readOnly={true} />
+    </Container>
+  );
 };
 
 export default Review;
 
 // style
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  padding: 0 20px;
+`;
+
+const ReviewDesc = styled.div`
+  width: 100%;
+  padding: 0 10px;
+
+  display: flex;
+  justify-content: space-between;
+
+  font-size: 15px;
+
+  button {
+    color: #3db44a;
+    text-decoration: underline;
+  }
+`;
