fee74ca8ecfca98f8e14f8c5532609df43d7dc90
alwn8918 <alwn8918@gmail.com>
2024-11-23 09:32:27 +0900

    ✨ Feat: 저장 페이지 기본 디자인

--- src/pages/Save/SavePage.style.ts
+++ src/pages/Save/SavePage.style.ts
@@ -8,4 +8,25 @@ export const Container = styled.div`
   min-height: 100vh;
 
   background-color: #faf9f1;
+  font-family: "NanumSquareNeoBold";
+  color: #282828;
+`;
+
+export const ListContainer = styled.div`
+  width: 333px;
+  height: 40px;
+  margin: 10px 0;
+
+  background-color: #ffffff;
+  border-radius: 20px;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  padding: 0 20px;
+  justify-content: space-evenly;
+  align-items: center;
+
+  font-family: "NanumSquareNeoExtraBold";
+  font-size: 13px;
+  color: #cccccc;
 `;
--- src/pages/Save/SavePage.tsx
+++ src/pages/Save/SavePage.tsx
@@ -1,11 +1,41 @@
 import React from "react";
 import * as S from "./SavePage.style";
 import Tabbar from "../../components/Tabbar";
+import TitleLine from "../../components/TitleLine";
+import SaveSubway from "./components/SaveSubway";
 
 const SavePage = () => {
+  const dummyData = [
+    {
+      name: "서울숲",
+      address: "서울 성동구 뚝섬로 273",
+      tags: ["숲", "숲길"],
+    },
+    {
+      name: "뚝섬유원지",
+      address: "서울 성동구 뚝섬로 200",
+      tags: ["유원지", "한강"],
+    },
+    {
+      name: "성수동 카페거리",
+      address: "서울 성동구 성수이로 147",
+      tags: ["카페", "거리"],
+    },
+    {
+      name: "성수동 카페거리",
+      address: "서울 성동구 성수이로 147",
+      tags: ["카페", "거리"],
+    },
+  ];
+
   return (
     <S.Container>
-      <p>SavePage</p>
+      <TitleLine title="저장한 길" />
+      <S.ListContainer>
+        <button>내가 좋아하는 길</button>
+        <button>내가 가고 싶은 길</button>
+      </S.ListContainer>
+      <SaveSubway subway="뚝섬역" data={dummyData} />
       <Tabbar />
     </S.Container>
   );
