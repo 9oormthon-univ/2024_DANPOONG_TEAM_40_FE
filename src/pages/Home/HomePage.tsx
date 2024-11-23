30f7db443d05eb61b2e9a20f85a773c4ceed22c9
sangmaaaaan <matthew10164@gmail.com>
2024-11-23 23:40:47 +0900

    refactor: add recent visit

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -9,6 +9,8 @@ import { BarrierFreeRecommendItem } from "../Recommend/components/RecommendItem"
 import BarrierFreeRecommendImage from "../../assets/recommendThumbnail.png"
 import { getContentByMonth } from "../Recommend/RecommendPage";
 import Search from "../../components/Search";
+import barrierRecommendReviewerProfile from "../../assets/barrierRecommendReviewerProfile.png";
+import map from "../../assets/icon/icon_map.svg";
 
 const HomePage = () => {
   const { icon } = getContentByMonth();
@@ -22,7 +24,10 @@ const HomePage = () => {
       <TitleLine title={"모두의 길"} />
       <Search />
       <SubTitleLine title={"노선도"} />
-      <S.RecentVisitTitle>최근 방문한 장소</S.RecentVisitTitle>
+      <div className="w-full flex flex-row pl-[20px]" >
+        <img src={map} alt="map_icon" className="w-[10px] h-[12px] my-auto mx-4"/>
+        <span className=" flex flex-col font-NanumSquareNeo font-Bold font-[12px]">최근 방문한 장소</span>
+      </div>
       <S.SubwayMapContainer>
         {/* Subway 이미지 */}
         <S.SubwayImage src={subwayLine2} alt="subwayLine2" />
@@ -51,7 +56,16 @@ const HomePage = () => {
             />
           </div>
         </div>
-      <Tabbar />
+       <div className="bg-white rounded-lg mt-10 min-w-[353px]">
+        <div className="flex flex-row p-10 w-full">
+          <img src={barrierRecommendReviewerProfile} alt="recommendThumbnail"/>
+          <div className="flex flex-col justify-start items-start ml-10">
+            <span className="font-NanumSquareNeo font-extrabold text-[15px]">햄스터</span>
+            <span className="font-NanumSquareNeo font-regular text-[12px] text-center mt-4">휠체어 타고 크리스마스 느끼기에 딱이에요!</span>
+        </div>
+
+      </div>
+      </div>
       <Tabbar />
     </R.Container>
   );
